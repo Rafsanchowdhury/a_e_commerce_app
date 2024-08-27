93ae1cdaab87d94b4027858dcbbb7508164409cb
rafsan chowdhury <rafsanc90@gmail.com>
2024-08-27 21:58:31 +0600

    Day 5 completed

--- lib/screens/user-panel/main-screen.dart
+++ lib/screens/user-panel/main-screen.dart
@@ -1,12 +1,12 @@
 // ignore: file_names
-// ignore_for_file: prefer_const_constructors, file_names, duplicate_ignore, prefer_const_literals_to_create_immutables, unused_local_variable
+// ignore_for_file: prefer_const_constructors, file_names, duplicate_ignore, prefer_const_literals_to_create_immutables, unused_local_variable, unused_import, avoid_unnecessary_containers
 
-import 'package:a_e_commerce_app/screens/auth-ui/welcome-screen.dart';
-import 'package:a_e_commerce_app/utils/app-constant.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:a_e_commerce_app/widgets/banner-widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import '../../utils/app-constant.dart';
+import '../../widgets/custom-drawer-widget.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -15,24 +15,28 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        iconTheme: IconThemeData(color: AppConstant.appTextColor),
         backgroundColor: AppConstant.appMainColor,
-        title: Text(AppConstant.appMainName),
+        title: Text(
+          AppConstant.appMainName,
+          style: TextStyle(color: AppConstant.appTextColor),
+        ),
         centerTitle: true,
-        actions: [
-          GestureDetector(
-            onTap: () async {
-              GoogleSignIn googleSignIn = GoogleSignIn();
-              FirebaseAuth _auth = FirebaseAuth.instance;
-              await _auth.signOut();
-              await googleSignIn.signOut();
-              Get.offAll(() => WelcomeScreen());
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(Icons.logout),
-            ),
-          )
-        ],
+      ),
+      drawer: DrawerWidget(),
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Container(
+          child: Column(
+            children: [
+              SizedBox(
+                height: Get.height / 90.0,
+              ),
+              //banner
+              BannerWidget(),
+            ],
+          ),
+        ),
       ),
     );
   }
