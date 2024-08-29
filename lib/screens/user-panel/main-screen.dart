4bb0afb7a318671efcf2854c590408b3ed5b15dd
rafsan chowdhury <rafsanc90@gmail.com>
2024-08-29 18:22:21 +0600

    Day 7 Completed

--- lib/screens/user-panel/main-screen.dart
+++ lib/screens/user-panel/main-screen.dart
@@ -1,13 +1,16 @@
 // ignore: file_names
 // ignore_for_file: prefer_const_constructors, file_names, duplicate_ignore, prefer_const_literals_to_create_immutables, unused_local_variable, unused_import, avoid_unnecessary_containers
 
+import 'package:a_e_commerce_app/screens/user-panel/all-categories-screen.dart';
 import 'package:a_e_commerce_app/widgets/banner-widget.dart';
 import 'package:a_e_commerce_app/widgets/heading-widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import '../../utils/app-constant.dart';
+import '../../widgets/category-widget.dart';
 import '../../widgets/custom-drawer-widget.dart';
+import '../../widgets/flash-sale-widget.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -40,15 +43,20 @@ class MainScreen extends StatelessWidget {
               HeadingWidget(
                 headingTitle: "Categories",
                 headingSubTitle: "According to your budget",
-                onTap: () {},
+                onTap: () => Get.to(() => AllCategoriesScreen()),
                 buttonText: "See More >",
               ),
+
+              CategoriesWidget(),
+
+              //heading
               HeadingWidget(
                 headingTitle: "Flash Sale",
                 headingSubTitle: "According to your budget",
                 onTap: () {},
                 buttonText: "See More >",
               ),
+              FlashSaleWidget(),
             ],
           ),
         ),
